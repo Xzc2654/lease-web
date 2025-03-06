9bfda2a29e05ebeecc479a2f536577dc5b65b016
Xzc2654 <26548183062@qq.com>
2025-03-06 18:04:10 +0800

    隐藏配置文件

--- web/web-admin/src/main/java/com/xzc/lease/web/admin/controller/apartment/FeeController.java
+++ web/web-admin/src/main/java/com/xzc/lease/web/admin/controller/apartment/FeeController.java
@@ -4,9 +4,12 @@ package com.xzc.lease.web.admin.controller.apartment;
 import com.xzc.lease.common.result.Result;
 import com.xzc.lease.model.entity.FeeKey;
 import com.xzc.lease.model.entity.FeeValue;
+import com.xzc.lease.web.admin.service.FeeKeyService;
+import com.xzc.lease.web.admin.service.FeeValueService;
 import com.xzc.lease.web.admin.vo.fee.FeeKeyVo;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,15 +20,22 @@ import java.util.List;
 @RequestMapping("/admin/fee")
 public class FeeController {
 
+    @Autowired
+    private FeeKeyService feeKeyService;
+    @Autowired
+    private FeeValueService feeValueService;
+
     @Operation(summary = "保存或更新杂费名称")
     @PostMapping("key/saveOrUpdate")
-    public Result saveOrUpdateFeeKey(@RequestBody FeeKey feeKey) {
+    public Result<?> saveOrUpdateFeeKey(@RequestBody FeeKey feeKey) {
+        feeKeyService.saveOrUpdate(feeKey);
         return Result.ok();
     }
 
     @Operation(summary = "保存或更新杂费值")
     @PostMapping("value/saveOrUpdate")
-    public Result saveOrUpdateFeeValue(@RequestBody FeeValue feeValue) {
+    public Result<?> saveOrUpdateFeeValue(@RequestBody FeeValue feeValue) {
+        feeValueService.saveOrUpdate(feeValue);
         return Result.ok();
     }
 
@@ -33,18 +43,19 @@ public class FeeController {
     @Operation(summary = "查询全部杂费名称和杂费值列表")
     @GetMapping("list")
     public Result<List<FeeKeyVo>> feeInfoList() {
-        return Result.ok();
+        List<FeeKeyVo> list = feeKeyService.feeInfoList();
+        return Result.ok(list);
     }
 
     @Operation(summary = "根据id删除杂费名称")
     @DeleteMapping("key/deleteById")
-    public Result deleteFeeKeyById(@RequestParam Long feeKeyId) {
+    public Result<?> deleteFeeKeyById(@RequestParam Long feeKeyId) {
         return Result.ok();
     }
 
     @Operation(summary = "根据id删除杂费值")
     @DeleteMapping("value/deleteById")
-    public Result deleteFeeValueById(@RequestParam Long id) {
+    public Result<?> deleteFeeValueById(@RequestParam Long id) {
         return Result.ok();
     }
 }
